78ebc5d729434638c4c431cf230fbfae2e756833
JoshuaCleverley <joshuacleverley@protonmail.com>
2022-05-31 01:00:07 +0100

    map + collision

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,88 @@ use tcod::colors::*;
 use tcod::console::*;
 
 const SCREEN_HEIGHT: i32 = 50;
-const SCREEN_WIDTH:  i32 = 80;
+const SCREEN_WIDTH: i32 = 80;
 
-const LIMIT_FPS:     i32 = 20;
+const LIMIT_FPS: i32 = 20;
 
-const MAP_HEIGHT:    i32 = 45;
-const MAP_WIDTH:     i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+const MAP_WIDTH: i32 = 80;
+
+const COLOR_DARK_WALL: Color = 
+    Color { 
+        r: 0, 
+        g: 0, 
+        b: 100 
+    };
+const COLOR_DARK_GROUND: Color = 
+    Color { 
+        r: 50, 
+        g: 50, 
+        b: 150 
+    };
+
+fn main() {
+    let root = 
+        Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Libtcod roguelike tutorial")
+        .init();
+    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
+
+    let mut tcod = Tcod { root, con };
+
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let player = Object::new(SCREEN_WIDTH / 2,     SCREEN_HEIGHT / 2, '@', WHITE);
+    let npc    = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
+
+    let mut objects = [player, npc];
+
+    let map = make_map();
+    let game = Game { map };
+
+    while !tcod.root.window_closed() {
+        tcod.con.clear();
+
+        render_all(&mut tcod, &game, &objects);
+
+        tcod.root.flush();
+        tcod.root.wait_for_keypress(true);
+        
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut tcod, &game, player);
+        if exit {
+            break;
+        }
+    }
+}
+
+fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
+    use tcod::input::Key;
+
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        Key { code: tcod::input::KeyCode::Up,     ..} => player.move_by( 0, -1, game),
+        Key { code: tcod::input::KeyCode::Down,   ..} => player.move_by( 0,  1, game),
+        Key { code: tcod::input::KeyCode::Left,   ..} => player.move_by(-1,  0, game),
+        Key { code: tcod::input::KeyCode::Right,  ..} => player.move_by( 1,  0, game),
+        Key { code: tcod::input::KeyCode::Escape, ..} => return true,
+        Key { code: tcod::input::KeyCode::Enter, alt: true, ..} => {
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        },
+        _ => {},
+    } false
+}
 
 struct Tcod {
     root: Root,
     con: Offscreen,
 }
 
+#[derive(Debug)]
 struct Object {
     x: i32,
     y: i32,
@@ -26,9 +96,11 @@ impl Object {
         Object { x, y, char, color }
     }
 
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
-        self.x += dx;
-        self.y += dy;
+    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
+        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
     }
 
     pub fn draw(&self, con: &mut dyn Console) {
@@ -37,67 +109,68 @@ impl Object {
     }
 }
 
-fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
-    use tcod::input::Key;
-
-    let key = tcod.root.wait_for_keypress(true);
-    match key {
-        Key { code: tcod::input::KeyCode::Up,     ..} => player.move_by( 0, -1),
-        Key { code: tcod::input::KeyCode::Down,   ..} => player.move_by( 0,  1),
-        Key { code: tcod::input::KeyCode::Left,   ..} => player.move_by(-1,  0),
-        Key { code: tcod::input::KeyCode::Right,  ..} => player.move_by( 1,  0),
-        Key { code: tcod::input::KeyCode::Escape, ..} => return true,
-        Key { code: tcod::input::KeyCode::Enter, alt: true, ..} => {
-            let fullscreen = tcod.root.is_fullscreen();
-            tcod.root.set_fullscreen(!fullscreen);
-        },
-        _ => {},
-    } false
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
 }
 
-fn main() {
-    let root = 
-        Root::initializer()
-        .font("arial10x10.png", FontLayout::Tcod)
-        .font_type(FontType::Greyscale)
-        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
-        .title("Libtcod roguelike tutorial")
-        .init();
-    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    let mut tcod = Tcod { root, con };
-
-    tcod::system::set_fps(LIMIT_FPS);
-
-    let player = Object::new(SCREEN_WIDTH / 2,     SCREEN_HEIGHT / 2, '@', WHITE);
-    let npc    = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
-
-    let mut objects = [player, npc];
-
-    while !tcod.root.window_closed() {
-        tcod.con.clear();
-        
-        for object in &objects {
-            object.draw(&mut tcod.con);
-        }
-
-        blit(
-            &tcod.con,
-            (0, 0),
-            (SCREEN_WIDTH, SCREEN_HEIGHT),
-            &mut tcod.root,
-            (0, 0),
-            1.0,
-            1.0,
-        );
-
-        tcod.root.flush();
-        tcod.root.wait_for_keypress(true);
-        
-        let player = &mut objects[0];
-        let exit = handle_keys(&mut tcod, player);
-        if exit {
-            break;
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false,
         }
     }
+
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
+    }
+}
+
+type Map = Vec<Vec<Tile>>;
+
+struct Game {
+    map: Map,
+}
+
+fn make_map() -> Map {
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+
+    map
+}
+
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+    for object in objects {
+        object.draw(&mut tcod.con);
+    }
+
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = game.map[x as usize][y as usize].block_sight;
+            if wall {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+
+    blit(
+        &tcod.con,
+        (0, 0),
+        (MAP_WIDTH, MAP_HEIGHT),
+        &mut tcod.root,
+        (0, 0),
+        1.0,
+        1.0,
+    );
 }
